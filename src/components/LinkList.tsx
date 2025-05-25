8a18a10b0b7f250f5d86828ebad63596052271d8
iyoshi-rgb <yoshito.i.2002@gmail.com>
2025-05-25 23:33:44 +0900

    feat: subscription

--- src/components/LinkList.tsx
+++ src/components/LinkList.tsx
@@ -1,10 +1,32 @@
 import { useQuery } from '@apollo/client';
 import Link, { type LinkType } from './Link';
 import { FEED_QUERY } from '../graphql/query';
-
+import { NEW_LINKS_SUBSCRIPTION } from '../graphql/subscription';
 
 function LinkList() {
-  const { data } = useQuery(FEED_QUERY)
+  const result = useQuery(FEED_QUERY);
+  const { data, subscribeToMore } = result;
+
+  subscribeToMore({
+    document: NEW_LINKS_SUBSCRIPTION,
+    onError: (err) => console.error('Subscription error:', err),
+    updateQuery: (prev, { subscriptionData }) => {
+      if (!subscriptionData.data) return prev;
+      const newLink = subscriptionData.data.newLink;
+      const exists = prev.feed.links.find(
+        ({ id }: { id: string }) => id === newLink.id
+      );
+      if (exists) return prev;
+
+      return Object.assign({}, prev, {
+        feed: {
+          links: [newLink, ...prev.feed.links],
+          count: prev.feed.links.length + 1,
+          __typename: prev.feed.__typename
+        }
+      });
+    }
+  });
 
   return (
     <div>
