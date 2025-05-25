421939c30692ba6a665849243593861214fc90c5
iyoshi-rgb <yoshito.i.2002@gmail.com>
2025-05-25 21:36:50 +0900

    feat: add ApolloClient middleware

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,25 @@ import './styles/index.css'
 import App from './components/App.tsx'
 import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client'
 import { BrowserRouter } from 'react-router'
+import { setContext } from '@apollo/client/link/context';
+import { AUTH_TOKEN } from './constants.ts'
 
 const httpLink = createHttpLink({
   uri: 'http://localhost:4000',
 })
 
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem(AUTH_TOKEN);
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ''
+    }
+  };
+});
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 })
 
