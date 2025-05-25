306c0fc97a638fa45b2b349b69239446990562e1
iyoshi-rgb <yoshito.i.2002@gmail.com>
2025-05-25 20:36:07 +0900

    feat: post mutation

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,13 @@
 // import '../styles/App.css'
+import CreateLink from './CreateLink'
 import LinkList from './LinkList'
 
 function App() {
   return (
-    <LinkList />
+    <>
+      <LinkList />
+      <CreateLink />
+    </>
   )
 }
 
